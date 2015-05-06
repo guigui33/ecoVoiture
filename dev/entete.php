898f54bbd25a5df9c81f9750a43e38fc0a402e20
guigui33 <guillaumecantal@gmail.com>
2015-05-06 08:20:55 +0200

    premier commit

---
+++ dev/entete.php
@@ -0,0 +1,51 @@
+<?php
+$titre=isset($_GET['titre'])?$_GET['titre']:'';
+session_start();
+	if(!isset($_SESSION['logged']) || !$_SESSION['logged']){
+		$connexion=true;
+	}
+	else {
+		$connexion=true;
+		$nom=$_SESSION['nom'];
+		}
+	
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><?php echo $titre; ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<!--<link rel="stylesheet" href="mystyle.css"> -->
+</head>
+<body>
+	<header>
+		<div>
+			<img src="logoEcoVoiture.jpg" alt="logo Eco Voiture" width="600px" height="100px" style="border:45px black;"/>
+		</div>
+		<div>
+		<img src="banniereEcoVoiture.jpg" alt="banniereEcoVoiture" width="600px" height="100px" style="border:45px black;"/>
+		</div>
+		<div>
+			<?php if($connexion==false){?>
+			Se connecter: </br>
+			<form action="verificationCompte.php" method="POST">
+			 Identifiant	Mot de passe <br/>
+			 <input type="login" name="login"> <br/>
+			 <input type="password" name="password"> <br/>
+			 <input type="submit" value="Connexion">
+			</form>
+			<a href="inscription.php?titre=inscription">Pas encore inscrit?</a>
+			<?php }			
+			else{?>
+				Bienvenue <?php echo $nom;?>
+				<button type="button" onclick="href='mesAnnonces.php?titre=\'Annonces\''">Mes annonces</button>
+				<button type="button" onclick="href='monProfil.php?titre=\'profil\''">Mon profil</button>
+				<button type="button" onclick="href='mesReservations.php?titre=\'reservation\''">Mes reservations</button>
+				<button type="button" onclick="href='seDeconnecter.php'">Se deconnecter</button>
+			<?php } ?>
+			</div>
+	</header>
+
+
+
---
+++ dev/inscription.php
@@ -0,0 +1,8 @@
+<?PHP
+include("entete.php");//inclus le fichier entete
+?>
+
+<h2> Inscription </h2>
+<form action=>
+
+</form>
