2cab95aa56fdc46cbcfd71aa67ecd78beccf8451
guigui33 <guillaumecantal@gmail.com>
2015-05-06 15:41:44 +0200

    ajout formulaire

--- fichiersPHP/inscription.php
+++ fichiersPHP/inscription.php
@@ -23,5 +23,15 @@ Last name:<br>
 	Indiquez une adresse email valide: <input type="email" name="email"><br/>
 	Choisissez un nom d'utilisateur: <input type="email" name="email"><br/>
 	Choisissez un mot de passe: <input type="password" name="psswd"><br/>
+	confirmer mot de passe: <input type="password" name="psswdBis"><br/>
 	</fieldset>
+	<fieldset>
+	<legend>Informations Personnelles</legend>
+	Nom: <input type="text" name="nom"> Téléphone: <input type="tel" name="telephone"> <br/>
+	Prénom: <input type="text" name="prenom"> Date de naissance: <input type="date" name="date"> <br/>
+	Adresse: <input type="field" name="adresse"><br/>
+	Code postal: <input type="number" name="codePostal"><br/>
+	Ville: <input type="text" name="ville"><br/>
+	</fieldset>
+	<button type="button">Je m'inscris!'</button>
 </form>
