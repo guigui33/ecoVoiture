1bcac44e8828c18bfd3a4d783a1f82afd50fe9ba
guigui33 <guillaumecantal@gmail.com>
2015-05-09 22:54:38 +0200

    correction bug footer

--- fichiersPHP/entete_footer.php
+++ fichiersPHP/entete_footer.php
@@ -51,13 +51,13 @@ function entete($titre){ ?>
 <?php } 
 
 function footer(){ ?>
-	</body>
 	<footer>
 	<button type="button" onclick="href='a_propos.php'">A propos d'Ecovoiture</button>
 	<button type="button" onclick="href='foire_questions.php'">Foire aux questions</button>
 	<button type="button" onclick="href='conditions_generales_utilisation.php'">Conditions générales d'utilisation</button>
 	<button type="button" onclick="href='nous_contacter.php'">Nous contacter</button>
 	</footer>
+	</body>
 	</html>
 <?php } ?>
 
