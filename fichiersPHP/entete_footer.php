ffcad9f3702c51fb15fe8cc0c54ec9ba98511343
guigui33 <guillaumecantal@gmail.com>
2015-05-12 14:13:23 +0200

    modif confli

--- fichiersPHP/entete_footer.php
+++ fichiersPHP/entete_footer.php
@@ -64,20 +64,17 @@ function entete($titre){ ?>
 <?php } 
 
 function footer(){ ?>
-<<<<<<< HEAD
 <center>
 <div class="footer">
 <br><a href="apropos.php">A propos d'EcoVoiture</a> | <a href="FAQ.php">Foire aux questions</a> | <a href="Condition.php">Condition géneral d'utilisation</a> | <a href="Nouscontacter.php">Nous contacter</a>	
 </div>
 </center>
-=======
 	<footer>
 	<button type="button" onclick="href='a_propos.php'">A propos d'Ecovoiture</button>
 	<button type="button" onclick="href='foire_questions.php'">Foire aux questions</button>
 	<button type="button" onclick="href='conditions_generales_utilisation.php'">Conditions générales d'utilisation</button>
 	<button type="button" onclick="href='nous_contacter.php'">Nous contacter</button>
 	</footer>
->>>>>>> 62c5ec76d80ffd58d5ca6e28094c2a48644ebf26
 	</body>
 	</html>	
 <?php } ?>
