5e9e146d45ae16a9281cbb46bfff02d4506c545b
TomZzy <thomas.massip32@gmail.com>
2015-05-25 18:39:16 +0200

    les ficheir concernant les trajets

---
+++ fichiersPHP/ajout_trajet.php
@@ -0,0 +1,41 @@
+<?php
+require('connexion.php');
+
+//verification des informations donnés par le client avant de les inclurent dans la BD
+
+
+$depart=isset($_POST['depart'])?$_POST['depart']:'';
+$destination=isset($_POST['destination'])?$_POST['destination']:'';
+$date = isset($_POST['date'])?$_POST['date']:'';
+$heure=isset($_POST['heure'])?$_POST['heure']:'';
+$place=isset($_POST['place'])?$_POST['place']:'';
+$bagages=isset($_POST['bagages'])?$_POST['bagages']:'';
+$information=isset($_POST['information'])?$_POST['information']:'';
+
+echo $date;
+
+if($depart=='' && $destination=='' && $date=='' && $heure=='' && $place=='' && $bagages='' && $information=''){
+
+		header('location:proposer_trajet.php?error=1');//si erreur on retourne error=1
+		exit;
+	}
+$queryidvilledest=pg_query($connexion,"SELECT idlieu FROM lieux WHERE LOWER (ville) LIKE LOWER('$destination') ");
+$queryidvilledepart=pg_query($connexion,"SELECT idlieu FROM lieux WHERE LOWER (ville) LIKE LOWER('$depart') ");
+				
+				if(pg_num_rows($queryidvilledest) === 0){
+					echo("La ville de destination. (Il ce peut qu'elle ne soit pas dans la BDD si c'est le cas faire une requete a l'administrateur)");
+					header('location:inscription.php?error=ville');
+					exit;
+				}
+				echo $queryidvilledest;
+				while ($row = pg_fetch_assoc($queryidvilledepart)) {
+							$IdDepart=$row['idlieu'];
+							}
+				while ($row = pg_fetch_assoc($queryidvilledest)) {
+							$IdDest=$row['idlieu'];
+							}	
+				pg_query($connexion,"INSERT INTO trajets (datedepart, heuredepart, placesdispo, taillebagages, infostrajet, idVoitureutilisee,iddepart, idarrivee) VALUES ('$date','$heure','$place','$bagages','$information',1,'$IdDepart','$IdDest');");//insert value dans table login
+				pg_close($connexion);
+				//header('location:home.php');
+			
+?>
---
+++ fichiersPHP/proposer_trajet.php
@@ -0,0 +1,45 @@
+<?php
+include('entete_footer.php'); 
+entete('home');
+
+?>
+<h2 class="alerte alert-info" align="center"> Proposer un trajet </h2>
+
+<form action="ajout_trajet.php" method="POST" >
+	<fieldset>
+	<legend align="center">Itinéraire</legend>
+	<p><label for="depart">Ville de depart:</label><input  type="text"   id="depart"name="depart"  required  /><br /></p>
+	<p><label for="destination">Ville d'arrivée :</label><input type="text" name="destination" id="destination" required/><br /></p>
+	<label for="detour">Detour:</label><input type="radio" name="detour" id="detour" />Oui <input type="radio" name="detour" />Non<br /></p>
+	</fieldset>
+	<fieldset >
+	<legend align="center">Informations Supplementaire</legend>
+	<p><label for="date">Date: </label><input type="date" name="date" required  value="<?php echo date('Y-m-d', strtotime(date('Y-m-d')));?>"/> <br/>
+	<p><label for="heure">Heure de depart:</label> <input type="text" name="heure" required /> 
+	<p><label for="place">Nombre de place:</label> <input type="text" name="place" /> <br/>
+	<!--<p><label for="voiture">Voiture utilisée: </label><SELECT name="voiture" size="1">
+<OPTION>Pas de bagage
+<OPTION>Petit
+<OPTION>Moyen
+<OPTION>Grand
+</SELECT>
+<br/>--> <br/>
+	<p><label for="bagages">Taille des bagages:</label> <SELECT name="bagage" size="1">
+<OPTION>Pas de bagage
+<OPTION>Petit
+<OPTION>Moyen
+<OPTION>Grand
+</SELECT>
+<br/>
+	<p><label for="information">Autre information:</label> <textarea name="information" required /></textarea><br/>
+	</fieldset>
+    <center>
+	<input  type="checkbox" name="validerCondGeneral" value="validerCondGeneral"  required /> En cochant cette case, je certifie être étudiant et j'accepte 
+	<a href="condGeneralEcovoiture.php">les conditions générales d'utilisation d'Ecovoiture</a> <br/><br/>
+	<input class="btn btn-default" type="submit" value="Valider Trajet" /></input></center>
+</form>
+
+<?php 
+
+footer();
+?>
