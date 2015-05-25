5784f5ed28c84193c56a2358a04c6f9ef1bb2dd6
Yohann31 <yohann.legovic.94@gmail.com>
2015-05-25 22:52:21 +0200

    Creation page a propos

---
+++ fichiersPHP/a_propos.php
@@ -0,0 +1,32 @@
+<?PHP
+require_once("entete_footer.php");//inclus le fichier entete
+entete('A propos');
+?>
+
+<h2 style="background:#46bcde"> A propos de Ecovoiture </h2>
+
+<body>
+<div id="container">
+	<div id="contenu">	
+		<h4>Titre 1</h4>
+		<div>
+			blablabla
+		</div>
+		<br>
+		
+		<h4>Titre 2</h4>
+		<div>
+			blablabla
+		</div>
+		<br>
+		
+		<h4>Titre 3</h4>
+		<div>
+			blablabla
+		</div>
+		<br>
+	</div>
+</div>
+</body>
+
+<?php footer(); ?>
