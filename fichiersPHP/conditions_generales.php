5f7e886ca07b5e6ec7550ee985d2f9b4fc1ac940
Yohann31 <yohann.legovic.94@gmail.com>
2015-05-25 22:52:42 +0200

    Creation conditions generale utilisations

---
+++ fichiersPHP/conditions_generales.php
@@ -0,0 +1,122 @@
+<?PHP
+require_once("entete_footer.php");//inclus le fichier entete
+entete('Conditions générales d utilisation');
+?>
+
+<h2 style="background:#46bcde"> Conditions generales d'utilisations </h2>
+
+
+
+<body>
+<div id="container">
+	<div id="contenu">	
+		<ol>
+			<h4><li>Les condition générales d'utilisations</li></h4>
+			<div>
+				En utilisant le site pour effectuer un trajet en covoiturage, vous donnez votre accord pour respecter les « Conditions générales d'utilisations » décrite
+				ci-après. Ces conditions constitue des obligations à appliquer par les conducteurs et les passagers.
+			</div>
+			<br>
+			
+			
+			<h4><li>Les trajets en covoiturage</li></h4>
+			<div>
+				Le conducteur garantit de ne publier sur le site que des trajets qu’il souhaite réellement effectuer. Les condutcteurs doivent fournir mutuellement toute les information liées à 
+				l'itinéraire (points de départ et d'arrivée compris), à la présence ou tolérance d'animaux, à la présence ou tolérance de fumeurs et à la musique dans la voiture, et de 
+				manière générale, toutes informations à propos de la logistique du trajet, du confort et de toute autre préférence.
+
+				<br><br>Le conducteur garantit qu'il effectuera le trajet avec les personnes qui ont réservé et en prenant en compte les informations publiés sur le site de son trajet. Et lees passagers doivent s'assurer
+				que les informations fournient sont exactes qu'ils sont en accord la réservation effectuée et les condition générales d'utilisation. Ecovoiture n'est pas responsable des informations et des accords convenus entre utilisateurs.
+			</div>
+			<br>
+			
+			
+			<h4><li>La sécurité</li></h4>
+			<div>
+				Chaque conducteur garantit qu'il ne prendra aucun risque en conduisant et qu'il ne consommera ni alcool, ni drogues, ni médicaments pouvant altérer ou perturber sa capacité à 
+				conduire sans danger. Le passager ne devra pas perturber la conduite du conducteur.
+
+				<br><br>L'identité des personnes qui sont communiquées sur le site n'est pas vérifié par Ecovoiture, alors soyez attentif aux informations qui vous sont fournis et dont vous fournissez sur le site. 
+				En cas de fausse informations diffusées sur notre site par un utilisateur, nous vous invitons à immédiatement signaler à aux administrateurs de Ecovoiture, en nous contactant via "<a href="nousContacter.php">Nous contacter</a>".
+				Si vous n'êtes pas certain de l'identité d'un utilisateur, prevenez nous quand même en signalant votre incertitude.
+
+				<br><br>Pour plus de sécurité, prevenez des proches de votre covoiturage et informez les personnes du trajet que vous informez des proche de la plaque du conducteur par exemple dans le cas d'un passager, 
+				ou de la carte d'identité des passagers dans le cas d'un conducteur. Les utilisateurs n'auront pas d'objection et ne seront pas offensés par cette anticipation d’assurer sa propre sécurité. 
+				De plus, Ecovoiture recommande aux personnes du trajet de se retrouver le jours de rendez-vous du covoiturage dans une zone publique fréquentée comme un super marché.
+			</div>	
+			<br>
+			
+			
+			<h4><li>La transparaence</li></h4>
+			<div>
+				Les utilisateurs doivent fournir à toute autre personne les informations et les documents qui peuvent etre solliciter par autruis dans le cadre d'un covoiturage. 
+				Pour le conducteur, ces informations sont par exemple la carte grise, le certificat d'assurance, le contrôle technique et le permis de conduire. 
+				Pour toutes personnes c'est la carte d'identité, ou un passeport qui peut être demandé pour prouver son identité.
+
+				<br><br>L'identité de chaque personne doit être la même que celle publié sur Ecovoiture. Sans justificatif d'identité, il est conseillé de refuser le covoiturage.
+
+				<br><br>Ecovoiture n'est pas un site devant être utilisé à des fins commerciales ou professionnelles car c'est un site fondé sur l'entraide.
+			</div>	
+			<br>
+			
+			
+			<h4><li>Le coûts</li></h4>
+			<div>
+				Les passagers ne doivent payer aucun frais car le but premier du site est sur l'entraide et la solidarité des etudiants. Si un conducteur vous demande de payer une contribution, chaque passager 
+				se doit de refuser et venir nous le signaler en nous contactant via "<a href="nousContacter.php">Nous contacter</a>". Veuillez nous transmettre son nom et son prénom pour que nous puissons prendre des mesures.
+			</div>	
+			<br>
+			
+			
+			<h4><li>La ponctualité</li></h4>
+			<div>
+				Toutes les personnes doivent être présent au rendez-vous à l'heure et au lieu indiqué sur le trajet. Après une attente de 30min, le conducteur est en droit de débuter son trajet. ou les passagers sont en droit de le faire 
+				signaler aux administrateur de Ecovoiture, en nous contactant via "<a href="nousContacter.php">Nous contacter</a>".
+			</div>
+			<br>
+			
+			
+			<h4><li>Les règles et législations</li></h4>
+			<div>
+				Le conducteur doit prouver que son véhicule est en accord avec le Code de la Route et autre règle ou législation s'appliquant à leur conduite et à son véhicule comme par exemple 
+				les limitations de vitesses et les restrictions de chargement du véhicule. 
+				Le véhicule doit disposer d'un contrôle technique en cours de validité, d'un entretien régulier et aucun défaut pouvant porter atteinte à la sécurité des passagers. 
+				Le conducteur doit assuré pour la conduite du véhicule utilisé pour ce trajet et pour les passagers qui seront transportés.
+
+				<br><br>Chaque personne s'engage à ne pas porter ou transporter une substance, matière ou objet à caractère illégale, dangereuse ou offensante durant un covoiturage.
+			</div>
+			<br>
+			
+			
+			<h4><li>La propreté</li></h4>
+			<div>
+				Chaque personnes doit s'assurer de sa propreté et de celle des objets qu'il transporte au moment du départ et pendant le trajet, afin de ne pas offenser ou incommoder toute autre personne.
+			</div>
+			<br>
+			
+			
+			<h4><li>Le confort et les condition</li></h4>
+			<div>
+				Les personnes doivent trouver un accord sur les conditions suivantes avant le trajet et ces conditions constitueront une partie de l'accord établi entre eux: l'heure de départ, 
+				la quantité de bagages admise, le nombre de personnes qui voyagent, l'autorisation ou non de fumer, la présence d'animaux (et le cas échéant le type d'animaux accepté ou non), 
+				la présence de musique à bord (et comment la musique sera choisie), le point de rendez-vous et de dépose..
+			</div>
+			<br>
+			
+			
+			<h4><li>Le contenu des utilisateur</li></h4>
+			<div>
+				Chaque utilisateur est seul responsable de ce qu'il met en ligne sur le site. Il s’engage notamment à ce que ces données ne soient pas de nature à porter atteinte aux intérêts légitimes des tiers.
+				Ces données ne doivent pas être diffamatoires, offensantes, insultantes .. ou porter atteinte au bon fonctionnement du site (virus par exemple). En cas de non conformité, Ecovoiture est dans le droit 
+				de supprimer toutes informations contraires aux conditions générales d'utilisations.
+				
+				<br><br>Les utilisateurs s'engage à respecter les conditions générales d'utilisations énumérées ci-dessus.
+			</div>
+			<br>
+			
+		</ol>
+	</div>
+</div>
+</body>
+
+<?php footer(); ?>
