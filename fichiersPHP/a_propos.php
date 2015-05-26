4f51d25f2fe61a22cb5598a66bff95ba3b293b2c
Yohann31 <yohann.legovic.94@gmail.com>
2015-05-26 21:28:09 +0200

    Contenu ajouter

--- fichiersPHP/a_propos.php
+++ fichiersPHP/a_propos.php
@@ -8,21 +8,38 @@ entete('A propos');
 <body>
 <div id="container">
 	<div id="contenu">	
-		<h4>Titre 1</h4>
+		<h4>Qui somme nous ?</h4>
 		<div>
-			blablabla
+			Nous sommes 4 étudiants qui avons décidé de mettre en place une plateforme de covoiturage gratuit pour les étudiants. Nous utilisons l’hébergeur « Alwaysdata » pour notre base de données ainsi que notre site web. 
+			Nous avons développé ce site de nos propres manières du début jusqu’à la fin, pouvant modifier ou améliorer les services proposés.
 		</div>
 		<br>
 		
-		<h4>Titre 2</h4>
+		<h4>Le but de "Ecovoiture"</h4>
 		<div>
-			blablabla
+			Ecovoiture est comme cité plus haut, totalement gratuit dont l’objectif est de solliciter la solidarité des étudiants pour permettre une entraide. Cela signifie différents points à nos yeux :
+			
+			<ul TYPE="circle">
+			<li>Aider les étudiants qui ont besoins de se déplacer pour une raison personnelle et ayant peu de moyens financiers. Cela peut rapidement devenir compliqué dans notre société où il est demandé de 
+			payer sa part des que l’on souhaite faire quelque chose
+			
+			<li>Renouer des liens sociaux entre les personnes et faire de nouvelles rencontres. Il faut s’ouvrir et faire le pas pour faire de nouvelles rencontres et nouer de nouveaux liens. Facebook, Tweeter et autres réseaux 
+			sociaux ne sont pas les seuls moyens d’interaction sociale, de même pour les rencontres amoureuses de site spécialisé, durant un covoiturage il est possible de faire La rencontre que vous attendez depuis longtemps !!
+			
+			<li>Mais aussi faire une action écologique pour la planète quand de nos jours, la pollution est un sujet quotidien de nos vie. Au lieu que chaque personne prenne sa voiture seul et fasse son trajet, autant remplir 
+			cette même voiture et permette d’éviter la consommation de carburant de 3 autres véhicule ou plus.
+			</ul>
 		</div>
 		<br>
 		
-		<h4>Titre 3</h4>
+		<h4>Le futur de "Ecovoiture"</h4>
 		<div>
-			blablabla
+			Nous souhaitons proposé un service de qualité tout sans oublier nos objectifs de gratuité et d’écologie. Nous restons à votre écoute et vos avis seront d’une aide précieuse afin de mieux satisfaire de vos attentes. Si vous avez des idées, que ce soit dans le design, des options, des services, des manques ou toutes autres partie que vous pensez de nécessaires, contactez-nous via le formulaire de contact dont l'accès se trouve en bas de chaque page en cliquant sur "<a href="nousContacter.php">Nous contacter</a>".
+
+			<br><br>Un message de la part de toute notre équipe : <b>nous vous remercions d’utiliser notre plateforme et sommes reconnaissant pour votre confiance.</b>
+
+			<br><br><center><b>Ecovoiture</b></center>
+
 		</div>
 		<br>
 	</div>
