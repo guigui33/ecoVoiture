7d222cc072a9f04dcc03e0e50a15de5897a7f377
josselynL <loulier.j@gmail.com>
2015-05-26 10:10:18 +0200

    profil user

---
+++ fichiersPHP/profil_utilisateur.php
@@ -0,0 +1,19 @@
+<?php include('entete_footer.php'); 
+
+entete('Profil de ...');
+
+?>
+<center>
+<legend> Profil de ... :  </legend></center>
+
+<TABLE width="60%"> 
+<Td style="border:solid 1px black;">
+Evaluation moyenne du conducteur : 
+<br>
+Preferences pendant un trajet : 
+<br>
+Je covoiture depuis le : 
+<br>
+Ma voiture preferee : 
+</Td>
+</TABLE> 
