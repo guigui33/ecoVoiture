6a16b7872e4877918f31aba5ccb199f6e8f9b074
SIMON LAMOURE <Simon@macbook-pro-de-simon.home>
2015-05-27 18:51:53 +0200

    adaptation conditions générales

--- fichiersPHP/conditions_generales.php
+++ fichiersPHP/conditions_generales.php
@@ -9,16 +9,12 @@ entete('Conditions générales d utilisation');
 
 <body>
 <div id="container">
-	<div id="contenu">	
+	<div id="contenu">
+        <div>
+            En utilisant le site pour effectuer un trajet en covoiturage, vous donnez votre accord pour respecter les « Conditions générales d'utilisations » décrites ci-après. Ces conditions constituent des règles à appliquer par les conducteurs et les passagers.
+        </div>
 		<ol>
-			<h4><li>Les conditions générales d'utilisation</li></h4>
-			<div>
-				En utilisant le site pour effectuer un trajet en covoiturage, vous donnez votre accord pour respecter les « Conditions générales d'utilisations » décrites ci-après. Ces conditions constituent des règles à appliquer par les conducteurs et les passagers.
-			</div>
-			<br>
-			
-			
-			<h4><li>Les trajets en covoiturage</li></h4>
+			<h4><li>Les Trajets en covoiturage</li></h4>
 			<div>
 				Le conducteur garantit de ne publier sur le site que des trajets qu’il souhaite réellement effectuer. Les condutcteurs doivent fournir mutuellement toute les informations liées à l'itinéraire (points de départ et d'arrivée compris), à la présence ou tolérance d'animaux, à la présence ou tolérance de fumeurs et à la musique dans la voiture, et de manière générale, toutes information à propos de la logistique du trajet, du confort et de toute autre préférence.
 
@@ -27,7 +23,7 @@ entete('Conditions générales d utilisation');
 			<br>
 			
 			
-			<h4><li>La sécurité</li></h4>
+			<h4><li>La Sécurité</li></h4>
 			<div>
 				Chaque conducteur garantit qu'il ne prendra aucun risque en conduisant et qu'il ne consommera ni alcool, ni drogues, ni médicaments pouvant altérer ou perturber sa capacité à conduire sans danger. Le passager ne devra pas perturber la conduite du conducteur.
 
@@ -39,7 +35,7 @@ entete('Conditions générales d utilisation');
 			<br>
 			
 			
-			<h4><li>La transparaence</li></h4>
+			<h4><li>La Transparaence</li></h4>
 			<div>
 				Les utilisateurs doivent fournir les informations et documents qui peuvent être sollicités par autrui dans le cadre d'un covoiturage. Pour le conducteur, ces informations sont par exemple la carte grise, le certificat d'assurance, le contrôle technique et le permis de conduire. Pour toute autre personne il peut s'agir d'une pièce d'identité en cours de validité.
 
@@ -50,21 +46,21 @@ entete('Conditions générales d utilisation');
 			<br>
 			
 			
-			<h4><li>Le coût</li></h4>
+			<h4><li>Le Coût</li></h4>
 			<div>
 				Les passagers d'un covoiturage ne doivent pas subir de frais. Si un conducteur demande de payer une contribution, chaque passager se doit de refuser et de nous le signaler en nous contactant via la page "<a href="nousContacter.php">Nous contacter</a>". Veuillez nous transmettre son identité afin que nous puissons prendre des mesures.
 			</div>	
 			<br>
 			
 			
-			<h4><li>La ponctualité</li></h4>
+			<h4><li>La Ponctualité</li></h4>
 			<div>
-				Toutes les personnes doivent être présentes au rendez-vous à l'heure et au lieu indiqué sur le trajet. Le conducteur est en droit de refuser un passager si ce dernier se présente avec un retard trop important. Les passagers sont en droit de signaler aux administrateurs d'Ecovoiture tout conducteur refusant des passagers de manière abusive en nous contactant via la page"<a href="nousContacter.php">Nous contacter</a>". Les passagers sont également en droit d'annuler un trajet si le conduteur se présente plus d'une heure après l'heure de départ prévue.
+				Toutes les personnes doivent être présentes au rendez-vous à l'heure et au lieu indiqué sur le trajet. Le conducteur est en droit de refuser un passager si ce dernier se présente avec un retard trop important. Les passagers sont en droit de signaler aux administrateurs d'Ecovoiture tout conducteur refusant des passagers de manière abusive en nous contactant via la page "<a href="nousContacter.php">Nous contacter</a>". Les passagers sont également en droit d'annuler un trajet si le conduteur se présente plus d'une heure après l'heure de départ prévue.
 			</div>
 			<br>
 			
 			
-			<h4><li>Les règles et législations</li></h4>
+			<h4><li>Les Règles et Législations</li></h4>
 			<div>
 				Le conducteur doit prouver qu'il est en accord avec le Code de la Route et toute autre règle de droit s'appliquant à sa conduite et à son véhicule. 
 				Le véhicule doit disposer d'un contrôle technique en cours de validité, d'un entretien régulier et aucun défaut pouvant porter atteinte à la sécurité des passagers. 
@@ -73,16 +69,22 @@ entete('Conditions générales d utilisation');
 				<br><br>Chaque usager s'engage à ne pas détenir ou transporter une substance, matière ou objet à caractère illégal, dangereux ou offensant durant un covoiturage.
 			</div>
 			<br>
+            
+            <h4><li>La Juridiction</li></h4>
+			<div>
+				En tant que de besoin, la juridiction compétente en cas de litige sera considérée comme étant les tribunaux compétents soumis à la législation française.
+                
+                <br><br>Toute réclamation en relation contre Ecovoiture avec ses utilisateurs se prescrit dans un délai d'un an. Il est par conséquent présumé que le conducteur ou passager qui n'aura pas porté réclamation dans un délai d'un an renonce à son droit de déposer une plainte.
+			</div>
+			<br>
 			
-			
-			<h4><li>La propreté</li></h4>
+			<h4><li>La Propreté</li></h4>
 			<div>
 				Chaque usager doit s'assurer de sa propreté et de celle des objets qu'il transporte au moment du départ et pendant le trajet, afin de ne pas offenser ou incommoder toute autre personne.
 			</div>
 			<br>
 			
-			
-			<h4><li>Le confort et les conditions</li></h4>
+			<h4><li>Le Confort et les Conditions</li></h4>
 			<div>
 				Les usagers doivent trouver un accord sur les conditions suivantes avant le trajet et ces conditions constitueront une partie de l'accord établi entre eux: l'heure de départ, la quantité et la taille des bagages admises, le nombre de passagers, l'autorisation ou non de fumer, la présence d'animaux (et le cas échéant le type d'animaux acceptés ou non), 
 				la présence et les modalités de choix de musique à bord, le point de rendez-vous et d'arrivée...
@@ -90,12 +92,34 @@ entete('Conditions générales d utilisation');
 			<br>
 			
 			
-			<h4><li>Le contenu</li></h4>
+			<h4><li>Le Contenu</li></h4>
 			<div>
 				Chaque utilisateur est seul responsable des informations qu'il à lui-même mises en ligne sur le site. Il s’engage notamment à ce que ces données ne soient pas de nature à porter atteinte aux intérêts légitimes des tiers.
 				Ces données ne doivent pas être diffamatoires, offensantes, insultantes ... ou porter atteinte au bon fonctionnement du site. En cas de non conformité, Ecovoiture se réserve le droit de supprimer toutes informations contraires aux conditions générales d'utilisation.
-				
-				<br><br>Les utilisateurs s'engagent à respecter les conditions générales d'utilisation énumérées ci-dessus.
+			</div>
+            <h4><li>Les Données Personnelles</li></h4>
+			<div>
+				Conformément aux dispositions de la loi n°78-17 du 6 janvier 1978 modifiée par la loi n°2004-801 du 6 août 2004 relative à l'informatique, aux fichiers et aux libertés, ont seuls accès à vos données personnelles les administrateurs d'Ecovoiture, dont la tâche consiste à faire fonctionner le site ou à rendre le service.
+                
+                <br><br>Les données indiquées comme obligatoires dans le formulaire permettant de devenir utilisateur du site et du service nécessitent une réponse exacte de votre part. Tout défaut de réponse ou toute réponse jugée anormale par Ecovoiture est susceptible d'entrainer le refus d'Ecovoiture de prendre en compte votre demande d'inscription au service.
+                
+                <br><br>Les données collectées par Ecovoiture dans le cadre des services rendus sur le Site sont traitées conformément aux dispositions de la norme simplifiée n°48 et de la loi « Informatique et Libertés » du 6 août 2004.
+                
+                <br><br>Conformément aux dispositions de la norme simplifiée n°48, Ecovoiture se réserve le droit de transmettre tout ou partie des données personnelles concernant les Membres dans le strict respect de la norme simplifiée n°48 et de la loi « Informatique et Libertés » du 6 Août 2004. Tout usage de données personnelles autre que celui indiqué ci-dessus devra faire l'objet d'un consentement individuel, préalable et explicite de la part des Membres.
+
+                <br><br>Conformément à la loi n°78-17 du 6 janvier 1978, vous disposez d'un droit d'accès et de rectification sur les données personnelles vous concernant, en vous adressant au webmaster du Site, sauf pour les données personnelles que vous avez données à Ecovoiture lorsque vous avez rempli votre formulaire d'adhésion en ligne et qu'il vous appartient seul de modifier et de mettre à jour.
+                
+                <br><br>Conformément à la loi n°78-17 du 6 janvier 1978, vous disposez du droit de vous opposer sans frais et gratuitement, à ce que les données personnelles vous concernant soient utilisées à des fins de prospection, notamment commerciale, par Ecovoiture ou par ses partenaires commerciaux. Si votre droit d'opposition est exercé pour ce motif directement auprès d'Ecovoiture, ce dernier s'engage à répercuter votre opposition auprès de ses partenaires contractuels auxquels il aurait éventuellement transmis vos données personnelles.
+                
+                <br><br>Conformément à la loi n°78-17 du 6 janvier 1978, Ecovoiture s'engage à ce que les données personnelles vous concernant, même celles transmises à d'éventuels partenaires commerciaux, ne soient pas transmises hors de l'Union Européenne, sauf à recueillir au préalable votre consentement exprès en ce sens.
+
+                <br><br>Conformément à la loi n°78-17 du 6 janvier 1978, Ecovoiture se réserve le droit de transmettre les données personnelles vous concernant, soit pour respecter une obligation légale, soit en application d'une décision judiciaire, administrative, ou d'une autorité administrative indépendante (comme par exemple la Commission Nationale de l'Informatique et des Libertés).
+                
+                <br><br>Vous vous engagez à assurer, en tant que de besoin, la mise à jour des données personnelles vous concernant. Vous vous engagez en outre à ce que les données personnelles vous concernant soient exactes, complètes et non équivoques. Vous pouvez à tout moment accéder, en utilisant sur le site votre mot de passe et votre login, à votre "<a href="profil_utilisateur.php">Compte Utilisateur</a>" contenant l'ensemble des données personnelles que vous avez fournies à Ecovoiture.
+                
+                <br><br>Conformément à la loi n°78-17 du 6 janvier 1978, Ecovoiture s'engage à prendre toute précaution utile, au regard de la nature des données et des risques présentés par le traitement, pour préserver la sécurité des données personnelles vous concernant et, notamment, empêcher que vos données personnelles ne soient déformées, endommagées, ou que des tiers non autorisés y aient accès.
+                
+				<br><br><h5>Les utilisateurs s'engagent à respecter les conditions générales d'utilisation énumérées ci-dessus.</h5>
 			</div>
 			<br>
 			
