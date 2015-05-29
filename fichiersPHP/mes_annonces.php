e51d1ffe87f3307300176a06513442f044736823
guigui33 <guillaumecantal@gmail.com>
2015-05-29 17:46:52 +0200

    correction

--- fichiersPHP/mes_annonces.php
+++ fichiersPHP/mes_annonces.php
@@ -14,7 +14,8 @@ entete('Mes Annonces');
 require('connexion.php');
 
 //Fonction permettant d'afficher les annonces proposées par l'utilisateur
-function afficherTrajet($tabTrajet){	
+function afficherTrajet($tabTrajet)
+{	
 	$idlieux=$tabTrajet['iddepart'];
 	$depart=pg_query($GLOBALS['connexion'],"SELECT ville FROM lieux WHERE idlieu =$idlieux") ;
 	$depart=pg_fetch_assoc($depart);
@@ -35,8 +36,8 @@ function testDateHeure($date){
 	return $now<$date;		
 	}	
 	
-$iduser=$_SESSION['iduser'];
-$queryIdTrajetProposer=pg_query($connexion,"SELECT * FROM proposer WHERE idchauffeur='$iduser'");
+$iduser=$_SESSION['iduser']; //recupere l'identifiant de l'utilisateur, session demarrer dans la fonction entete()
+$queryIdTrajetProposer=pg_query($connexion,"SELECT * FROM proposer WHERE idchauffeur='$iduser'"); //recupere les trajets où l'utilisateur est chauffeur
 	
 ?>
 
@@ -46,21 +47,19 @@ $queryIdTrajetProposer=pg_query($connexion,"SELECT * FROM proposer WHERE idchauf
 		<div class="legend"> En cours </div>
 		<fieldset class="fieldset1">
 		<?php 
-		$i=0;
-		
+		$i=0;//utilisée si l'utilisateur n'a pas de trajet en cours		
 		//On parcours le tableau afin d'afficher les annonces proposées en cours
 		while(($tabIdTrajets=pg_fetch_assoc($queryIdTrajetProposer))){
 			$i++;
 			$idTrajet=$tabIdTrajets['idroute'];
 			$queryTrajetInfo=pg_query($connexion,"SELECT * FROM trajets WHERE idtrajet='$idTrajet'");
 			$tabTrajet=pg_fetch_assoc($queryTrajetInfo);
-			
+			/*si le trajet à une date anterieure on ne l'affiche pas*/
 			if(testDateHeure($tabTrajet['datedepart'].' '.$tabTrajet['heuredepart'])){
 				afficherTrajet($tabTrajet);	
 				echo '<hr>';
 			}
-		}
-		
+		}		
 		if($i==0){
 			echo "<center> Aucune annonce passée </center>";
 		}
@@ -70,14 +69,14 @@ $queryIdTrajetProposer=pg_query($connexion,"SELECT * FROM proposer WHERE idchauf
 		<fieldset class="fieldset1">
 		<?php 
 		$queryIdTrajetProposer=pg_query($connexion,"SELECT * FROM proposer WHERE idchauffeur='$iduser'");
-		$i=0;
-		
-		//On parcours le tableau afin d'afficher les annonces terminées
+		$i=0;		
+		//On parcours le tableau afin d'afficher les annonces terminées où l'utilisateur a été chauffeur
 		while(($tabIdTrajets=pg_fetch_assoc($queryIdTrajetProposer))){
 			$i++;
 			$idTrajet=$tabIdTrajets['idroute'];
 			$queryTrajetInfo=pg_query($connexion,"SELECT * FROM trajets WHERE idtrajet='$idTrajet'");
 			$tabTrajet=pg_fetch_assoc($queryTrajetInfo);
+			/*même fonctionnement mais affiche les parcours anterieurs*/
 			if(!testDateHeure($tabTrajet['datedepart'].' '.$tabTrajet['heuredepart'])){
 				afficherTrajet($tabTrajet);	
 				echo '<hr>';
